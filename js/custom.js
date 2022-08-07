aca2ff5a496e5056d4bf3590e41a63e54ff7e813
Colin-techdev <clarencecolin@gmail.com>
2022-08-07 18:06:53 -0400

    Initital Porfolio

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+(function($) {
+  'use strict';
+  $(function() {
+    // Code here executes When the DOM is loaded...
+  });
+  $(window).on('load', function() {
+    // Code here executes When the page is loaded
+  });
+}(jQuery));
